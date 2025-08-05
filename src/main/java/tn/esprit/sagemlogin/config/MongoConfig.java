e6db2b523bdbc3b42fe42a20be14f126f926ba88
wiem183 <wiem.errouissi@esprit.tn>
2025-08-05 10:14:34 +0100

    forth  commit- Spring Boot backend

--- src/main/java/tn/esprit/sagemlogin/config/MongoConfig.java
+++ src/main/java/tn/esprit/sagemlogin/config/MongoConfig.java
@@ -1,19 +1,19 @@
 package tn.esprit.sagemlogin.config;
 
-
-
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MongoConfig {
 
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
     @Bean
     public MongoClient mongoClient() {
-        // Remplace par l’URI de connexion réelle si nécessaire
-        return MongoClients.create("mongodb://sagem-mongo:27017/projets");
+        return MongoClients.create(mongoUri);
     }
 }
-
--- src/main/java/tn/esprit/sagemlogin/config/MongoProjetsConfig.java
+++ src/main/java/tn/esprit/sagemlogin/config/MongoProjetsConfig.java
@@ -2,6 +2,7 @@ package tn.esprit.sagemlogin.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -16,9 +17,12 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 )
 public class MongoProjetsConfig {
 
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
     @Bean(name = "projetsMongoTemplate")
     public MongoTemplate projetsMongoTemplate() {
-        MongoClient client = MongoClients.create("mongodb://sagem-mongo:27017/projets");
+        MongoClient client = MongoClients.create(mongoUri);
         MongoDatabaseFactory factory = new SimpleMongoClientDatabaseFactory(client, "projets");
         return new MongoTemplate(factory);
     }
